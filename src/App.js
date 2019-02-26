4e84c893fcf9af9df2b468138d10accd61f5fd62
sancarbar <sancarbar@gmail.com>
2019-02-26 18:29:41 -0500

    Fetch to retrieve books list.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,19 @@ import React, { Component } from 'react';
 class App extends Component {
 
 
-  com
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            booksList: [],
+        };
+    }
+
+    componentDidMount() {
+        fetch('https://www.googleapis.com/books/v1/volumes?q=bogota')
+            .then(response => response.json())
+            .then(data => this.setState({ booksList: data.items }));
+    }
 
   render() {
     return (
